14bb87decfdf9617d2dc1a6c1e835f945fff0849
MohammedBOUTEBAL <boutebal.mohammed2001@gmail.com>
2023-02-15 00:40:53 +0100

    issue-1 sauvegarder les infos du formulaire en BD

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,12 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -18,35 +20,33 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Votre Prenom',
+                'constraints' => new Length(30, 2),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre Prenom'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Votre Nom',
+                'constraints' => new Length(30, 2),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre Nom'
                 ]
                 ])
             ->add('email', EmailType::class, [
                 'label' => 'Votre email',
+                'constraints' => new Length(60, 2),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre email'
                 ]
                 ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Votre password',
-                'attr' => [
-                    'placeholder' => 'Merci de saisir votre password'
-                ]
+            ->add('password', RepeatedType::class, [
+                'type' =>  PasswordType::class,
+                'invalid_message' => 'Le mdp et la confirmation ne sont pas identiques',
+                'required' => true,
+                'first_options' => [    'label' => 'Mot de passe'],
+                'second_options' => [   'label' => 'La confirmation du Mot de passe']     
                 ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmez votre password',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'Merci de confirmer votre password'
-                ]
-                ])
+            
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire"
                 ])    
